3d08adac65d1ee851640637b1b7a6b8c953a6eca
copytenz <copytenz@users.noreply.github.com>
2017-12-09 00:51:36 +0300

    initial file

---
+++ tz_archive_dropbox.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# 
+################################################################################
+# SCRIPT: 	tz_archive_dropbox.sh
+#
+# DESCRIPTION:	Archive Dropbox Camera Uploads so that the space is not eaten
+#
+# USAGE: 	
+#
+# VERSION: 	0.1 
+# VERSION DATE:	2017-12-08
+# CHANGE BY:	4e
+# CHANGES:	* New script
+################################################################################
+
+workDir="/mnt/storage/Dropbox/Camera Uploads"
+screenShotsDir=/mnt/granary/manual-backup/screenshots
+photosDir=/mnt/granary/private/photos/CameraUploads
+
+cd "$workDir"
+
+for folder in "$screenShotsDir" "$photosDir"; do mkdir -p $folder; done
+
+function mv_files_to_targetFolder {
+	export  fileMask targetDir
+	[ -z "$1" ] && fileMask="*.jpg" || fileMask=$1 
+	[ -z "$2" ] && targetDir="$photosDir" || targetDir=$2 
+		
+	subDir=$(ls $fileMask 2> /dev/null | head -1)
+	if [ -z "$subDir" ]; then
+		echo "* No files with mask $fileMask"
+		return 1
+	fi
+	subDir=${subDir:0:7} 
+	targetDir=$targetDir/$subDir
+	echo "`date +%Y-%m-%d" "%H:%M:%S` * Archiving to folder $targetDir"
+	mkdir -p "$targetDir"
+	# echo "${subDir}${fileMask}" "$targetDir/" 
+	mv ${subDir}${fileMask} "$targetDir/"
+	return 0
+}
+
+[ -f *.png ] && mv *.png "$screenShotsDir/" || echo "* No PNG files in the folder"
+
+i=0
+while true; do
+	[ "$i" -gt 1000 ] && break
+
+	mv_files_to_targetFolder "*.jpg" "$photosDir"
+	[ "$?" -eq 1 ] && break
+
+	let i=$i+1
+done
+echo "Number of folder iterations for mask *.jpg is i=$i"
+
+i=0
+while true; do
+	[ "$i" -gt 1000 ] && break
+
+	mv_files_to_targetFolder "*.png" "$screenShotsDir"
+	[ "$?" -eq 1 ] && break
+
+	let i=$i+1
+done
+echo "Number of folder iterations for mask *.png is i=$i"
+
+echo "`date +%Y-%m-%d" "%H:%M:%S` * Finished archving"

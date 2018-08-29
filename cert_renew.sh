ce279a2a6501be417796e59d52c7b8d1620a07c9
copytenz <copytenz@yandex.ru>
2018-08-30 00:05:15 +0300

    20180830 added ssl_cert_update_certbot.sh

--- cert_renew.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-sudo certbot renew --dry-run --http-01-port 9080
---
+++ ssl_cert_update_certbot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export siteName="$1"
+if [ -z "$siteName" ] then exit
+
+if [ ! $(whoami) == "root" ]; then 
+	sudo $0; 
+	exit
+fi
+echo "* sleeping for 3 sec.."
+sleep 3
+
+# Renewing the certificate in common folder
+sudo certbot renew --http-01-port 9080 --tls-sni-01-port 9443
+
+# Renewing the cerficiate for Jira & confluence
+cd /etc/letsencrypt/live/$siteName/
+openssl pkcs12 -export -out bundle.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -password pass:MyHomeIsHere
+cp /opt/atlassian/bundle.pfx /opt/atlassian/bundle.pfx.$(date +%Y-%m-%d)
+cp /etc/letsencrypt/live/$siteName/bundle.pfx /opt/atlassian/
+chmod g+r /opt/atlassian/bundle.pfx
+
+echo "* sleeping for 3 sec.."
+sleep 3
+/etc/init.d/confluence restart
+/etc/init.d/jira stop
+/etc/init.d/jira start

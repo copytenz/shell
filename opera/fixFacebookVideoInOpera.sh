4b031d641d54c2c475b91f173cdd8fbcd160a7a6
copytenz <copytenz@yandex.ru>
2019-03-03 14:39:44 +0300

    opera fix script

--- opera/fixFacebookVideoInOpera.sh
+++ opera/fixFacebookVideoInOpera.sh
@@ -1,2 +1,3 @@
 # source: https://forums.opera.com/topic/27537/can-t-play-facebook-videos-or-gifs-in-opera-w-linux-mint/44
+sudo cp /usr/lib/x86_64-linux-gnu/opera/libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/libffmpeg.so.`date +%Y-%m-%d`
 sudo cp libffmpeg.so /usr/lib/x86_64-linux-gnu/opera/
